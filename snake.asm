225978651875654a61ae03427851550506b34365
THashimov <tosha.hashimov@googlemail.com>
2023-04-21 21:14:42 +0100

    graphical mode set

--- snake.asm
+++ snake.asm
@@ -2,6 +2,18 @@
 
 call disc_load
 
+mov ah, 0x0 ; Set graphical mode
+mov al, 0x13 ; 320x200 screen
+int 0x10
+
+draw_pixel:
+    mov ah, 0xC
+    mov dx, 50
+    mov cx, 50
+    mov al, 0xF
+    int 0x10
+
+
 cont_loop:
     jmp $
 
