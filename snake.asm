555409a49f3112414a757279093314d9f179762c
THashimov <tosha.hashimov@googlemail.com>
2023-04-23 16:45:58 +0100

    snake can move up and down. working on keypress next

--- snake.asm
+++ snake.asm
@@ -3,34 +3,87 @@
 call disc_load
 call init_start_point
 
+check_for_key:
+    jmp check_for_key
+    ; pusha
+    ; mov ah, 0
+    ; int 0x16
+    ; cmp al, 'w'
+    ; je change_direction
+    ; cmp al, 's'
+    ; je change_direction
+
+    ; jmp main_loop
+
+    ; change_direction:
+    ;     popa
+    ;     ; int 0x10
+    ;     mov byte [direction], al
+
+    ; main_loop:
+    ;     popa
+    ;     ; int 0x10
+    ;     cmp byte [direction], 's'
+    ;     jne up
+
+    ;     down:
+    ;         call move_down
+    ;         jmp check_for_key
+
+    ;     cmp byte [direction], 'w'
+    ;     jne down
+
+    ;     up:
+    ;         call move_up
+    ;         jmp check_for_key
+
+move_up:
+    call draw_snake_vert
+    call sleep
+    call clear_screen
+    call increment_head
+    ret
+    
+increment_head:
+    ; To give the illusion of the snake moving up, we need to move the head coord up, the snake will still be drawn top to bottom
+    push ax
+    mov al, -1 ; This needs to run 1 times more than the length of the snake otherwise the head will be the same as the start. This needs to go one square further up than the tail
+    for_each_square_inc:
+        sub dx, 12
+        inc al
+        cmp al, [len_of_snake]
+        jne for_each_square_inc
+    pop ax 
+    ret
+
 move_down:
-    call draw_snake_down
+    call draw_snake_vert
     call sleep
     call clear_screen
     call decrement_head
-    jmp move_down
+    ret
 
 decrement_head:
     ; For each square in the snake length, we sub 12 from dx
     ; With each move, the head moves 12 pixels. 10 for the body square and two to give a gap between squares
     push ax
     mov al, 1
-    for_each_square:
+    for_each_square_dec:
         sub dx, 12
         inc al
         cmp al, [len_of_snake]
-        jne for_each_square
+        jne for_each_square_dec
     pop ax 
     ret
 
-draw_snake_down:
+draw_snake_vert:
     call draw_square
     inc word [squares_drawn]
     push ax
     mov al, [squares_drawn]
     cmp al, [len_of_snake]
     pop ax
-    jc draw_snake_down
+    jc draw_snake_vert
     mov word [squares_drawn], 0
     ret
 
@@ -84,7 +137,6 @@ init_start_point:
     mov al, 0xF
     ret
 
-
 ; %include "print.asm"
 %include "read_disc.asm"
 
@@ -99,3 +151,5 @@ row_counter: dw 0
 
 len_of_snake: dw 3
 squares_drawn: dw 0
+
+direction: db 's'
